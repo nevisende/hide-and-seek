c0615944fcc9358132f56f10f1336a22255dcc34
Furkan Denizhan <84407337+nevisende@users.noreply.github.com>
2021-11-12 11:27:02 +0300

    Prepare for hide and seek exercise

---
+++ script.js
@@ -0,0 +1,162 @@
+/*  eslint linebreak-style: ["error", "unix"] */ /*  eslint-disable linebreak-style  */
+const hamburgerMenu = document.getElementById('hamburger-menu');
+const headerList = document.getElementById('header-list');
+const menuIcon = document.getElementById('menu-icon');
+const itemsHeader = document.querySelectorAll('.header-item');
+const projectButtons = document.querySelectorAll('.project-button');
+const popupContainer = document.querySelector('.popup-container');
+const closePopup = document.getElementById('close-popup');
+const popupTitle = document.getElementById('popup-title');
+const popupImg = document.querySelector('.popup-img');
+const popupImgList = document.querySelector('.images');
+const popupP = document.getElementById('popup-p');
+const technologiesList = document.querySelector('.popup-list');
+const liveUrlLink = document.getElementById('live-url');
+const sourceUrlLink = document.getElementById('source-url');
+
+const projects = [{
+  name: 'Project name goes here',
+  tecnologies: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
+  desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi expedita quos velit magni? Porro eos libero, numquam neque nisi facere eum modi a quos autem, facilis nihil hic at aliquid eaque error molestiae sit voluptatum assumenda beatae nostrum. Quas, delectus',
+  images: ['./media/project-1.png', './media/project-1.png', './media/project-1.png', './media/project-1.png'],
+  liveUrl: 'http://hackon.me',
+  sourceUrl: 'https://github.com/nevisende/my-portfolio',
+},
+{
+  name: 'Project name goes here',
+  tecnologies: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
+  desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi expedita quos velit magni? Porro eos libero, numquam neque nisi facere eum modi a quos autem, facilis nihil hic at aliquid eaque error molestiae sit voluptatum assumenda beatae nostrum. Quas, delectus',
+  images: ['./media/project-2.png', './media/project-2.png', './media/project-2.png', './media/project-2.png'],
+  liveUrl: 'http://hackon.me',
+  sourceUrl: 'https://github.com/nevisende/my-portfolio',
+},
+{
+  name: 'Project name goes here',
+  tecnologies: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
+  desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi expedita quos velit magni? Porro eos libero, numquam neque nisi facere eum modi a quos autem, facilis nihil hic at aliquid eaque error molestiae sit voluptatum assumenda beatae nostrum. Quas, delectus',
+  images: ['./media/project-3.png', './media/project-3.png', './media/project-3.png', './media/project-3.png'],
+  liveUrl: 'http://hackon.me',
+  sourceUrl: 'https://github.com/nevisende/my-portfolio',
+},
+{
+  name: 'Project name goes here',
+  tecnologies: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
+  desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi expedita quos velit magni? Porro eos libero, numquam neque nisi facere eum modi a quos autem, facilis nihil hic at aliquid eaque error molestiae sit voluptatum assumenda beatae nostrum. Quas, delectus',
+  images: ['./media/project-4.png', './media/project-4.png', './media/project-4.png', './media/project-4.png'],
+  liveUrl: 'http://hackon.me',
+  sourceUrl: 'https://github.com/nevisende/my-portfolio',
+},
+{
+  name: 'Project name goes here',
+  tecnologies: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
+  desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi expedita quos velit magni? Porro eos libero, numquam neque nisi facere eum modi a quos autem, facilis nihil hic at aliquid eaque error molestiae sit voluptatum assumenda beatae nostrum. Quas, delectus',
+  images: ['./media/project-5.png', './media/project-5.png', './media/project-5.png', './media/project-5.png'],
+  liveUrl: 'http://hackon.me',
+  sourceUrl: 'https://github.com/nevisende/my-portfolio',
+},
+{
+  name: 'Project name goes here',
+  tecnologies: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
+  desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi expedita quos velit magni? Porro eos libero, numquam neque nisi facere eum modi a quos autem, facilis nihil hic at aliquid eaque error molestiae sit voluptatum assumenda beatae nostrum. Quas, delectus',
+  images: ['./media/project-6.png', './media/project-6.png', './media/project-6.png', './media/project-6.png'],
+  liveUrl: 'http://hackon.me',
+  sourceUrl: 'https://github.com/nevisende/my-portfolio',
+}];
+
+function toogleMenu() {
+  headerList.classList.toggle('header-list');
+  hamburgerMenu.classList.toggle('hamburger-menu');
+  if (menuIcon.classList.contains('fa-bars')) {
+    menuIcon.classList.remove('fa-bars');
+    menuIcon.classList.add('fa-times');
+  } else {
+    menuIcon.classList.remove('fa-times');
+    menulcon.classList.add('fa-bars');
+  }
+}
+
+menuIcon.addEventListener('click', () => {
+  toogleMenu();
+});
+
+itemsHeader.forEach((item) => {
+  item.addEventListener('click', () => {
+    if (window.outerWidth <= 980) toogleMenu();
+  });
+});
+
+projectButtons.forEach((projectButton) => {
+  projectButton.addEventListener('click', () => {
+    const bigImageNumber = '0';
+    const projectNumber = projectButton.value;
+    let i;
+    let k;
+    popupTitle.innerText = projects[projectNumber - 1].name;
+    technologiesList.innerHTML = '';
+    popupImgList.innerHTML = '';
+
+    for (i = 0; i < projects[projectNumber - 1].tecnologies.length; i += 1) {
+      const tech = document.createElement('li');
+      tech.innerText = projects[projectNumber - 1].tecnologies[i];
+      technologiesList.appendChild(tech);
+    }
+    popupImg.src = projects[projectNumber - 1].images[bigImageNumber];
+
+    for (k = 0; k < projects[projectNumber - 1].images.length; k += 1) {
+      const image = document.createElement('img');
+      image.src = projects[projectNumber - 1].images[k];
+      image.classList.add('list-img');
+      popupImgList.appendChild(image);
+    }
+
+    popupP.innerText = projects[projectNumber - 1].desc;
+    liveUrlLink.href = projects[projectNumber - 1].liveUrl;
+    sourceUrlLink.href = projects[projectNumber - 1].sourceUrl;
+    popupContainer.style.display = 'flex';
+  });
+});
+
+closePopup.addEventListener('click', () => {
+  popupContainer.style.display = 'none';
+});
+// Local Storage
+
+function setInputsToLocalStorage() {
+  const nameContact = document.getElementById('name');
+  const messageContact = document.getElementById('message');
+  const emailContact = document.getElementById('email');
+  const localStorageArr = {
+    name: nameContact.value,
+    email: emailContact.value,
+    message: messageContact.value,
+  };
+  localStorage.setItem('contactForm', JSON.parse(localStorageArr));
+}
+
+const nameContact = document.getElementById('name');
+const messageContact = document.getElementById('message');
+const emailContact = document.getElementById('email');
+const inputArray = [nameContact, emailContact, messageContact];
+inputArray.forEach((input) => {
+  input.addEventListener('keyup', () => {
+    setInputsToLocalStorage();
+  });
+});
+
+function setInput() {
+  const nameContact = document.getElementById('name');
+  const messageContact = document.getElementById('message');
+  const emailContact = document.getElementById('email');
+  const localStorageArr = JSON.parse(localStorage.getItem('contactForm'));
+  if (localStorage.getItem('contactForm')) {
+    nameContact.value = localStorageArr.name;
+    messageContact.value = localStorageArr.message;
+    emailContact.value = localStorageArr.email;
+  }
+}
+
+function main() {
+  setInput();
+}
+
+window.onload = main();
